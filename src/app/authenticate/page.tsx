27c49d96dbfe9ec30489cc77b4378b3fd6dd442a
kristjanpeterson <kristjan@nftport.xyz>
2024-09-12 14:06:29 +0300

    Wrap /authenticate page in <Suspense/>

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -6,6 +6,15 @@ import {zxcvbn} from '@zxcvbn-ts/core';
 import {Suspense} from "react";
 
 export default function AuthenticatePage() {
+
+  return (
+    <Suspense>
+      <Authenticate/>
+    </Suspense>
+  )
+}
+
+function Authenticate() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -69,41 +78,39 @@ export default function AuthenticatePage() {
   }
 
   return (
-    <Suspense>
-      <div
-        className="flex w-full min-h-screen max-w-[1200px] mx-auto flex-col items-center gap-20 pt-10 py-2 px-5 lg:px-10 z-2 relative">
-        <div>Auth page</div>
+    <div
+      className="flex w-full min-h-screen max-w-[1200px] mx-auto flex-col items-center gap-20 pt-10 py-2 px-5 lg:px-10 z-2 relative">
+      <div>Auth page</div>
 
-        <div>Lets finish setting up your account</div>
-        <div className={"gal-error"}>{errorMessage}</div>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          {/*TODO: whatever fields required here*/}
-          {/*<input*/}
-          {/*  type="email"*/}
-          {/*  placeholder="email"*/}
-          {/*  value={email}*/}
-          {/*  onChange={(e) => setEmail(e.target.value)}*/}
-          {/*  className="border px-4 py-2 text-black"*/}
-          {/*/>*/}
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border px-4 py-2 text-black"
-          />
-          <input
-            type="password"
-            placeholder="Confirm password"
-            value={passwordConfirmation}
-            onChange={(e) => setPasswordConfirmation(e.target.value)}
-            className="border px-4 py-2 text-black"
-          />
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2">
-            Sign up
-          </button>
-        </form>
-      </div>
-    </Suspense>
+      <div>Lets finish setting up your account</div>
+      <div className={"gal-error"}>{errorMessage}</div>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        {/*TODO: whatever fields required here*/}
+        {/*<input*/}
+        {/*  type="email"*/}
+        {/*  placeholder="email"*/}
+        {/*  value={email}*/}
+        {/*  onChange={(e) => setEmail(e.target.value)}*/}
+        {/*  className="border px-4 py-2 text-black"*/}
+        {/*/>*/}
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border px-4 py-2 text-black"
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          className="border px-4 py-2 text-black"
+        />
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2">
+          Sign up
+        </button>
+      </form>
+    </div>
   )
 }
