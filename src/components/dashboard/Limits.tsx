b55a36dd72c969f660a8493bc74c32378c71a57d
kristjanpeterson <kristjan@nftport.xyz>
2024-10-24 10:54:03 +0300

    Limits page updates

--- src/components/dashboard/Limits.tsx
+++ src/components/dashboard/Limits.tsx
@@ -1,3 +1,4 @@
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
 import {DashboardContent} from "@/components/dashboard/components/DashboardContent";
 import {Title} from "@/components/Text";
 import {useEffect, useState} from "react";
@@ -36,7 +37,7 @@ export function Limits() {
       const responseJson = await response.json()
       console.log("responseJson")
       console.log(responseJson)
-      if (responseJson.rate_limit_minute && responseJson.rate_limit_day) {
+      if (responseJson.usage_tier_name && responseJson.usages !== null) {
         setRateLimits(responseJson)
       }
       // if (responseJson.api_key) setApiKey(responseJson.api_key)
@@ -54,68 +55,51 @@ export function Limits() {
           API usage stats & limits
         </div>
       </div>
-      <div className={"pt-8"}>
+      <div className={"py-8"}>
         {(isLoading && !rateLimits) &&
           <div>Loading...</div>
         }
         {rateLimits &&
           <>
+            <div className={"flex flex-col md:flex-row pb-8"}>
+              <LimitsCard
+                title="Usage tier"
+                isLoading={isLoading}
+                text={rateLimits.usage_tier_name ? rateLimits.usage_tier_name : ""}
+              />
+            </div>
+            <div
+              className={"py-8 px-3 md:px-8 flex flex-col gap-8 gal-card"}
+            >
+              <div>Daily API usage</div>
+              {rateLimits.usages.map((usage, index) => {
+                return <div key={index}>
+                  <div className={"gal-title-secondary pb-4"}>
+                    {usage.model}
+                  </div>
+                  <UsageBar
+                    usage={usage.requests_used_day}
+                    usageLeft={usage.requests_left_day}
+                    maxUsage={usage.max_requests_per_day}
+                    text={"requests"}
+                  />
+                  <UsageBar
+                    usage={usage.tokens_used_day}
+                    usageLeft={usage.tokens_left_day}
+                    maxUsage={usage.max_tokens_per_day}
+                    text={"tokens"}
+                  />
+                </div>
+              })}
+            </div>
             <div
               className={"flex flex-col pt-[32px] gap-6"}
             >
-              <div className={"flex flex-col md:flex-row gap-6"}>
-                <LimitsCard
-                  title="Usage tier"
-                  isLoading={isLoading}
-                  text={rateLimits.usage_tier_name ? rateLimits.usage_tier_name : ""}
-                />
-              </div>
               <div className={"px-3 pt-10 gal-text-secondary max-w-4xl"}>
-                Rate limits
-              </div>
-              <div className={"flex flex-col md:flex-row gap-6"}>
-                <LimitsCard
-                  title="Max requests"
-                  isLoading={isLoading}
-                  text={rateLimits.rate_limit_minute.max_requests}
-                  subText={"req/min"}
-                />
-                <LimitsCard
-                  title="Max tokens"
-                  isLoading={isLoading}
-                  text={rateLimits.rate_limit_minute.max_tokens}
-                  subText={"tok/min"}
-                />
-              </div>
-              <div className={"flex flex-col md:flex-row gap-6"}>
-                <LimitsCard
-                  title="Max requests"
-                  isLoading={isLoading}
-                  text={rateLimits.rate_limit_day.max_requests}
-                  subText={"req/day"}
-                />
-                <LimitsCard
-                  title="Max tokens"
-                  isLoading={isLoading}
-                  text={rateLimits.rate_limit_day.max_tokens}
-                  subText={"tok/day"}
-                />
-              </div>
-
-              <div
-                className={"py-8 px-3 md:px-8 flex flex-col gap-8 gal-card"}
-              >
-                <div>Daily API usage</div>
-                <UsageBar
-                  usageLeft={rateLimits.rate_limit_day.remaining_requests}
-                  maxUsage={rateLimits.rate_limit_day.max_requests}
-                  text={"requests"}
-                />
-                <UsageBar
-                  usageLeft={rateLimits.rate_limit_day.remaining_tokens}
-                  maxUsage={rateLimits.rate_limit_day.max_tokens}
-                  text={"tokens"}
-                />
+                <div className={"pb-4"}>
+                  Rate limits
+                </div>
+                <LimitsTable limits={rateLimits.usages || []}/>
               </div>
             </div>
 
@@ -170,21 +154,64 @@ function LimitsCard({title, text, isLoading, subText, iconName}: Props) {
 }
 
 function UsageBar(
-  {usageLeft, maxUsage, text}: {
-    usageLeft: number,
-    maxUsage: number,
+  {usage, usageLeft, maxUsage, text}: {
+    usage: number | null,
+    usageLeft: number | null,
+    maxUsage: number | null,
     text: string,
   }
 ) {
 
-  const percentage = Math.max(((maxUsage - usageLeft) / maxUsage) * 100, 1)
+  let percentage = 0
+  if (usageLeft !== null && maxUsage !== null && usageLeft !== maxUsage) {
+    percentage = Math.max(((maxUsage - usageLeft) / maxUsage) * 100, 1)
+  }
 
   return (
-    <div className={"flex flex-col gap-2"}>
+    <div className={"flex flex-col gap-2 pb-4"}>
       <div className={"gal-subtitle"}>
-        {formatNumber(maxUsage - usageLeft)} / {formatNumber(maxUsage)} {text}
+        {(maxUsage && usageLeft) ? formatNumber(maxUsage - usageLeft) : (usage || 0)} / {maxUsage ? formatNumber(maxUsage) : "∞"} {text}
       </div>
       <Progress value={percentage} className="w-full"/>
+    </div>
+  )
+}
+
+function LimitsTable({limits}: { limits: any[] }) {
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Model</TableHead>
+            {/*<TableHead>Tier</TableHead>*/}
+            {/*<TableHead>Price per 1M token</TableHead>*/}
+            <TableHead>RPM ¹</TableHead>
+            <TableHead>RPD ²</TableHead>
+            <TableHead>TPM ³</TableHead>
+            <TableHead>TPD ⁴</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {limits.map((limit) => (
+            <TableRow key={limit.model}>
+              <TableCell className="font-medium">{limit.model}</TableCell>
+              {/*<TableCell>{"TODO"}</TableCell>*/}
+              {/*<TableCell>-</TableCell>*/}
+              <TableCell>{limit.max_requests_per_minute ? formatNumber(limit.max_requests_per_minute) : "∞"}</TableCell>
+              <TableCell>{limit.max_requests_per_day ? formatNumber(limit.max_requests_per_day) : "∞"}</TableCell>
+              <TableCell>{limit.max_tokens_per_minute ? formatNumber(limit.max_tokens_per_minute) : "∞"}</TableCell>
+              <TableCell>{limit.max_tokens_per_day ? formatNumber(limit.max_tokens_per_day) : "∞"}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className={"gal-subtitle flex flex-col pt-4 gap-1"}>
+          <div>(1) Requests Per Minute</div>
+          <div>(2) Requests Per Day</div>
+          <div>(3) Tokens Per Minute</div>
+          <div>(4) Tokens Per Day</div>
+      </div>
     </div>
   )
 }
